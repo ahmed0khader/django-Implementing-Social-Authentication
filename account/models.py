a8711586f806c294c6f819a4b230eb6da56dcc9d
Ahmed Khader <81951957+ahmed0khader@users.noreply.github.com>
2023-07-07 15:33:55 +0300

    Update models.py

--- account/models.py
+++ account/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.conf import settings
-# إضافة الحقل التالي للمستخدم ديناميكيًا
 from django.contrib.auth import get_user_model
 # Create your models here.
 
@@ -12,11 +11,9 @@ class Profile(models.Model):
     def __str__(self):
         return f'Profile of {self.user.username}'
     
-# شابتر 7
 class Contact(models.Model):
     user_from = models.ForeignKey('auth.User', related_name='rel_from_set', on_delete=models.CASCADE)
     user_to = models.ForeignKey('auth.User', related_name='rel_to_set', on_delete=models.CASCADE)
-    # لتخزين الوقت الذي تم فيه إنشاء العلاقة
     created = models.DateTimeField(auto_now_add=True)
     # 
     
@@ -29,7 +26,5 @@ class Contact(models.Model):
         def __str__(self):
             return f'{self.user_from} follows {self.user_to}'
         
-# إضافة الحقل التالي للمستخدم ديناميكيًا
 user_model = get_user_model()
 user_model.add_to_class('following', models.ManyToManyField('self', through=Contact, related_name='followers', symmetrical=False))
-# متماثل = خطأ لتعريف علاقة غير متناظرة (إذا تابعتك ، فذلك لا يعني أنك تتابعني تلقائيًا). symmetrical=False
